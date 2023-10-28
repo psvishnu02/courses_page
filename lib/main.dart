bc77da5cad5f789a76915464dbf6b6a54227d835
psvishnu02 <vishnups2302@gmail.com>
2023-10-28 15:31:46 +0530

    flutter first project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+void main()
+{
+runApp(MaterialApp(home:Center(child:(Text("hello",
+  style: TextStyle(decoration: TextDecoration.none,color: Colors.yellow[800],
+      fontWeight: FontWeight.bold,
+      fontSize:80),)))));
+
+}
